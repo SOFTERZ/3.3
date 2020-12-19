2cb72b2a8383ad899c238068a1a7d4642a6b9e91
SOFTERZ <72169426+SOFTERZ@users.noreply.github.com>
2020-12-20 01:45:46 +0200

    Add files via upload

---
+++ sumN/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sumN
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }   
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {              
+                    double cymma = 0;
+                    int n = int.Parse(countBox1.Text);
+                    double x = double.Parse(countBox2.Text);
+                    for (int i = 0; i < n; i++)
+                    {
+                    cymma += Math.Sin(i *2 + 1 * x) / (i * 2 + 1);
+                    }
+                    resultBox.Text = $"Сумма {n} рядов = {cymma}";               
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
